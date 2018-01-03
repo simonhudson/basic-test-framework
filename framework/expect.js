228510955ba5bf734654e22838c3ec57e0cfc2af
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-01-03 08:40:29 +0000

    code structure

--- framework/expect.js
+++ framework/expect.js
@@ -2,13 +2,14 @@
 
 module.exports = (result) => {
 
-	const checkIsBoolean = () => result === true || result === false;
-
 	return {
 		toEqual(expected) {
 			if (result !== expected) throw new Error(`${result} is not equal to ${expected}`);
 		},
 		toBeType(expectedType) {
+
+			const checkIsBoolean = () => result === true || result === false;
+
 			let isType = typeof result === expectedType;
 			if (expectedType === 'boolean') isType = checkIsBoolean();
 			if (!isType) throw new Error(`${result} is not ${expectedType}`);
