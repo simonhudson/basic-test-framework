ba19ae0dde3403805e61d3b4c3af1efff65246e7
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-01-03 08:38:58 +0000

    add framework methods

--- framework/expect.js
+++ framework/expect.js
@@ -2,9 +2,16 @@
 
 module.exports = (result) => {
 
+	const checkIsBoolean = () => result === true || result === false;
+
 	return {
 		toEqual(expected) {
 			if (result !== expected) throw new Error(`${result} is not equal to ${expected}`);
+		},
+		toBeType(expectedType) {
+			let isType = typeof result === expectedType;
+			if (expectedType === 'boolean') isType = checkIsBoolean();
+			if (!isType) throw new Error(`${result} is not ${expectedType}`);
 		}
 	}
 
--- js/maths.test.js
+++ js/maths.test.js
@@ -6,41 +6,48 @@ const maths = require('./maths');
 test('sum() adds numbers', () => {
 	const result = maths.sum(5, 3);
 	const expected = 8;
+	expect(result).toBeType('number');
 	expect(result).toEqual(expected);
 });
 
 test('subtract() subtracts numbers', () => {
 	const result = maths.subtract(5, 3);
 	const expected = 2;
+	expect(result).toBeType('number');
 	expect(result).toEqual(expected);
 });
 
 test('multiply() multiplies numbers', () => {
 	const result = maths.multiply(5, 3);
 	const expected = 15;
+	expect(result).toBeType('number');
 	expect(result).toEqual(expected);
 });
 
 test('isEvenNumber() checks if number is even', () => {
 	const result = maths.isEvenNumber(26);
 	const expected = true;
+	expect(result).toBeType('boolean');
 	expect(result).toEqual(expected);
 });
 
 test('isEvenNumber() checks if number is even', () => {
 	const result = maths.isEvenNumber(25);
 	const expected = false;
+	expect(result).toBeType('boolean');
 	expect(result).toEqual(expected);
 });
 
 test('isOddNumber() checks if number is odd', () => {
 	const result = maths.isOddNumber(25);
 	const expected = true;
+	expect(result).toBeType('boolean');
 	expect(result).toEqual(expected);
 });
 
 test('isOddNumber() checks if number is odd', () => {
 	const result = maths.isOddNumber(26);
 	const expected = false;
+	expect(result).toBeType('boolean');
 	expect(result).toEqual(expected);
 });
