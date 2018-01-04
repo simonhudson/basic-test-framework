357202095ce50bfb157f302afc724803c454d443
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-01-04 08:37:40 +0000

    Add string method

--- js/string.js
+++ js/string.js
@@ -5,6 +5,12 @@ const uppercaseFirstChar = (string) => {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+const replaceSpaceWithDelimiter = (string, delimiter = '-') => {
+	if (!string || typeof string !== 'string') return string;
+	return string.replace(/\s+/g, delimiter);
+}
+
 module.exports = {
+	replaceSpaceWithDelimiter,
 	uppercaseFirstChar
 };
--- js/string.test.js
+++ js/string.test.js
@@ -4,16 +4,40 @@ const string = require('./string');
 test('uppercaseFirstChar() converts first character of string to uppercase', () => {
 
 	const tests = [
-		{ input: 1, expected: undefined },
-		{ input: [1,2,3], expected: undefined },
-		{ input: true, expected: undefined },
-		{ input: {}, expected: undefined },
 		{ input: 'lorem Ipsum', expected: 'Lorem Ipsum' }
 	];
 
 	tests.forEach(test => {
 		const { input, expected } = test;
 		const result = string.uppercaseFirstChar(input);
+		expect(result).toEqual(expected);
+	});
+
+});
+
+test('replaceSpaceWithDelimiter() replaces spaces in string with specified delimiter', () => {
+
+	const originalString = 'lorem Ipsum  dolor   foo';
+
+	const tests = [
+		{
+			input: {
+				string: originalString,
+			},
+			expected: 'lorem-Ipsum-dolor-foo'
+		},
+		{
+			input: {
+				string: originalString,
+				delimiter: '+'
+			},
+			expected: 'lorem+Ipsum+dolor+foo'
+		}
+	];
+
+	tests.forEach(test => {
+		const { input, expected } = test;
+		const result = string.replaceSpaceWithDelimiter(input.string, input.delimiter);
 		expect(result).toEqual(expected);
 	});
 
