7ad2b5bcbed033be9c8e1ceea1481b9d7d06bb9e
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-10-15 12:09:21 +0100

    error format

--- expect/expect.js
+++ expect/expect.js
@@ -4,7 +4,7 @@ module.exports = (result) => {
 
 	return {
 		toEqual(expected) {
-			if (result !== expected) throw new Error(`Actual (${result} | ${typeof result}) is not equal to expected (${expected} | ${typeof expected})`);
+			if (result !== expected) throw new Error(`\nActual (${result} | ${typeof result}) is not equal to expected (${expected} | ${typeof expected})\n`);
 		},
 	}
 
