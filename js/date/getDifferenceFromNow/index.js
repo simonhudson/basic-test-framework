eca1355e585c53916d10e8ccb4adf00b8f2072ef
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-10-16 15:34:56 +0100

    wip

--- js/date/getDifferenceFromNow/index.js
+++ js/date/getDifferenceFromNow/index.js
@@ -6,15 +6,10 @@ const moment = require('moment');
 const VALID_UNITS = ['seconds', 'minutes', 'hours', 'days', 'weeks', 'months', 'years'];
 
 module.exports = (date, unit) => {
+	console.log(typeof date, date);
 	if (!date || !unit || VALID_UNITS.indexOf(unit.toLowerCase()) < 0) return null;
 	date = new Date(date);
-	console.log(`>>> date: ${date}`);
 	if (!isValidDate(date)) return null;
 	const diff = Math.floor(moment.duration(moment().diff(date)).as(unit));
-	console.log('--------------------');
-	console.log(date);
-	console.log(diff);
-	console.log(unit);
-	console.log('--------------------');
 	return diff;
 };
--- js/date/getDifferenceFromNow/test.js
+++ js/date/getDifferenceFromNow/test.js
@@ -2,15 +2,23 @@
 
 const { expect, test } = require('../../../expect');
 const getDifferenceFromNow = require('./index');
+const getMonthName = require('../getMonthName');
 
-const NOW = new Date();
-const now = {
-	day: NOW.getUTCDate(),
-	month: NOW.getUTCMonth() + 1,
-	year: NOW.getUTCFullYear()
-};
 
 module.exports = () => {
+
+	const NOW = new Date();
+	const nowDate = {
+		day: NOW.getUTCDate(),
+		month: NOW.getUTCMonth() + 1,
+		year: NOW.getUTCFullYear()
+	};
+	const FUTURE_ONE_YEAR = `${nowDate.month} ${nowDate.day} ${nowDate.year + 1}`;
+	const FUTURE_TEN_YEARS = `${nowDate.month} ${nowDate.day} ${nowDate.year + 10}`;
+	const FUTURE_FOUR_MONTHS = `${nowDate.month + 4} ${nowDate.day} ${nowDate.year}`;
+	const PAST_ONE_YEAR = `${nowDate.month} ${nowDate.day} ${nowDate.year - 1}`;
+	const PAST_TEN_YEARS = `${nowDate.month} ${nowDate.day} ${nowDate.year - 10}`;
+	const PAST_FOUR_MONTHS = `${nowDate.month - 4} ${nowDate.day} ${nowDate.year}`;
 	
 	test('getDifferenceFromNow() gets difference from now as specified value', () => {
 	
@@ -18,14 +26,18 @@ module.exports = () => {
 			{ input: { date: null, unit: 'years' }, expected: null },
 			{ input: { date: 'hello', unit: 'years' }, expected: null },
 			{ input: { date: '13', unit: 'years' }, expected: null },
-			{ input: { date: `${now.day} ${now.month} ${now.year - 1}`, unit: 'years' }, expected: 1 },
-			{ input: { date: `${now.day} ${now.month} ${now.year - 19}`, unit: 'years' }, expected: 19 },
-			{ input: { date: `${now.day} ${now.month - 3} ${now.year}`, unit: 'months' }, expected: 3 },
-			{ input: { date: `${now.day + 10} ${now.month} ${now.year}`, unit: 'days' }, expected: 10 }
+			{ input: { date: FUTURE_ONE_YEAR, unit: 'years' }, expected: -1 },
+			{ input: { date: FUTURE_TEN_YEARS, unit: 'years' }, expected: -10 },
+			{ input: { date: FUTURE_FOUR_MONTHS, unit: 'months' }, expected: -4 },
+			{ input: { date: PAST_ONE_YEAR, unit: 'years' }, expected: 1 },
+			{ input: { date: PAST_TEN_YEARS, unit: 'years' }, expected: 10 },
+			{ input: { date: PAST_FOUR_MONTHS, unit: 'months' }, expected: 4 },
 		];
 	
 		tests.forEach(test => {
 			const { input, expected } = test;
+			console.log(`>>> ${input.date} / ${input.unit}`);
+			console.log();
 			const actual = getDifferenceFromNow(input.date, input.unit);
 			expect(actual).toEqual(expected);
 		});
