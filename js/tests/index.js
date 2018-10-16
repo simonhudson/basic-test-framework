efc57f9d8bbede776f8025b5ef2b6c70643b9c4e
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-10-16 08:51:15 +0100

    loop through tests

--- js/tests/index.js
+++ js/tests/index.js
@@ -1,18 +1,13 @@
 'use strict';
 
-require('../date/getMonthName/test')();
-require('../date/isValidDate/test')();
-require('../date/toIsoString/test')();
+const uppercaseFirstCharacter = require('../string/uppercaseFirstCharacter')
+const TESTS = {
+	date: ['getMonthName', 'isValidDate', 'toIsoString'],
+	maths: ['add', 'divide', 'getAverage', 'getPercentage', 'isEven', 'isOdd', 'multiply', 'subtract'],
+	string: ['replaceWhitespaceWithDelimiter', 'toTitleCase', 'uppercaseFirstCharacter']
+};
 
-require('../maths/add/test')();
-require('../maths/divide/test')();
-require('../maths/getAverage/test')();
-require('../maths/getPercentage/test')();
-require('../maths/isEven/test')();
-require('../maths/isodd/test')();
-require('../maths/multiply/test')();
-require('../maths/subtract/test')();
-
-require('../string/replaceWhitespaceWithDelimiter/test')();
-require('../string/toTitleCase/test')();
-require('../string/uppercaseFirstCharacter/test')();
+for (const key in TESTS) {
+	console.log(`\n${uppercaseFirstCharacter(key)}\n----------`);
+	TESTS[key].forEach(test => require(`../${key}/${test}/test`)());
+};
