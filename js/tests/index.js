d386466fe6bc78b0583166d7490d16fbfcd05956
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-10-17 09:39:47 +0100

    forma

--- js/tests/index.js
+++ js/tests/index.js
@@ -1,15 +1,36 @@
 'use strict';
 
-const uppercaseFirstCharacter = require('../string/uppercaseFirstCharacter');
-
 const TESTS = {
-	date: ['getDayName', 'getDifferenceBetweenDates', 'getMonthName', 'isOver18Years', 'isValidDate', 'toIsoString'],
-	maths: ['add', 'divide', 'getAverage', 'getPercentage', 'isEven', 'isOdd', 'multiply', 'subtract'],
-	string: ['beginsWithVowel', 'defineArticle', 'replaceWhiteSpaceWithDelimiter', 'stripWhiteSpace', 'toTitleCase', 'uppercaseFirstCharacter']
+	date: [
+		'getDayName',
+		'getDifferenceBetweenDates',
+		'getMonthName',
+		'isOver18Years',
+		'isValidDate',
+		'toIsoString'
+	],
+	maths: [
+		'add',
+		'divide',
+		'getAverage',
+		'getPercentage',
+		'isEven',
+		'isOdd',
+		'multiply',
+		'subtract'
+	],
+	string: [
+		'beginsWithVowel',
+		'defineArticle',
+		'replaceWhiteSpaceWithDelimiter',
+		'stripWhiteSpace',
+		'toTitleCase',
+		'uppercaseFirstCharacter'
+	]
 };
 
 for (const key in TESTS) {
-	console.log(`\n${uppercaseFirstCharacter(key)}\n----------`);
+	console.log(`\n${key}\n----------`);
 	TESTS[key].forEach(test => require(`../${key}/${test}/test`)());
 };
 console.log('\n');
