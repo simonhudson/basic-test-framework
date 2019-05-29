6de0c7f1d2e76dc0b5414e0ce4a2052eeff171a7
Simon Hudson <simon.hudson@experian.com>
2019-05-29 15:02:10 +0100

    update logging

--- expect/expect.js
+++ expect/expect.js
@@ -4,7 +4,7 @@ module.exports = (result) => {
 
 	return {
 		toEqual(expected) {
-			if (result !== expected) throw new Error(`\nActual (${result} | ${typeof result}) is not equal to expected (${expected} | ${typeof expected})\n`);
+			if (result !== expected) throw new Error(`\nActual (${JSON.stringify(result, null, 4)} | ${typeof result}) is not equal to expected (${JSON.stringify(expected, null, 4)} | ${typeof expected})\n`);
 		},
 	}
 
