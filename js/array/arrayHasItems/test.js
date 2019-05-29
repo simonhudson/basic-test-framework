567fcc0d12b1c80bceb22615702c50adf687d8fb
Simon Hudson <simon.hudson@experian.com>
2019-05-29 15:29:23 +0100

    array test

---
+++ js/array/arrayHasItems/test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { expect, test } = require('../../../expect');
+const arrayHasItems = require('./index');
+
+module.exports = () => {
+	
+	test('arrayHasItems() checks if array has items', function() {
+
+		const tests = [
+			{ input: null, expected: false },
+			{ input: 1, expected: false },
+			{ input: 'string', expected: false },
+			{ input: [], expected: false },
+			{ input: {}, expected: false },
+			{ input: [1], expected: true },
+			{ input: ['1', '2', 'lorem', true], expected: true }
+		];
+
+		tests.forEach(test => {
+			const { input, expected } = test;
+			const actual = arrayHasItems(input);
+			expect(actual).toEqual(expected);
+		});
+		
+	});
+
+};
--- js/tests/index.js
+++ js/tests/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const UTILITIES = [
+	'array',
 	'date',
 	'maths',
 	'string'
