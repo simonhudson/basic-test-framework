68089c591869b76e23b74b4b53d65b291dd52b8f
Simon Hudson <simon.hudson@experian.com>
2019-05-30 15:55:19 +0100

    heading update

--- expect/logHeading.js
+++ expect/logHeading.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = file => console.log(`\n${file}\n----------`);
+module.exports = file => console.log(`\n${file.charAt(0).toUpperCase() + file.slice(1)}\n----------`);
