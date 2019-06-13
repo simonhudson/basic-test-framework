ad2bcd42aa1e6436e51c3173c0d2cbc5ab84d319
Simon Hudson <simon.hudson@experian.com>
2019-06-13 09:55:26 +0100

    remove func()

--- expect/expect.js
+++ expect/expect.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const errorMessage = (expected, result) => `\nActual (${JSON.stringify(result, null, 4)} | ${typeof result}) is not equal to expected (${JSON.stringify(expected, null, 4)} | ${typeof expected})\n`;
-
 const performCheck = (expected, result) => {
-	if (result !== expected) throw new Error(errorMessage(expected, result));
+	if (result !== expected) throw new Error(
+		`\nActual (${JSON.stringify(result, null, 4)} | ${typeof result}) is not equal to expected (${JSON.stringify(expected, null, 4)} | ${typeof expected})\n`
+	);
 };
 
 module.exports = (result) => {
